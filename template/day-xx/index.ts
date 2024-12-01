26b0a976929645de7755a6bce0c09fdafd2b219f
Shadow <hi@shadowing.dev>
2024-12-01 02:22:53 -0600

    More refinements bc we're in bun now

--- template/day-xx/index.ts
+++
@@ -1,7 +0,0 @@
-export const part1 = (input: string) => {
-	return parseInt(input)
-}
-
-export const part2 = (input: string) => {
-	return parseInt(input)
-}
--- turbo/generators/templates/day/runner.ts
+++ turbo/generators/templates/day/runner.ts
@@ -1,13 +1,7 @@
 import { part1, part2 } from "./logic.js"
 
-const run = async () => {
-	const input = await Bun.file(`inputs/puzzle.txt`).text()
-	console.log(
-		`========================== Day {{day}} ==========================`
-	)
-	console.log(`                   ${input.split("\n").length} lines in input`)
-	console.log(`                   Part 1: ${part1(input)}`)
-	console.log(`                   Part 2: ${part2(input)}`)
-}
-
-run()
+const input = await Bun.file(`inputs/puzzle.txt`).text()
+console.log(`========================== Day {{day}} ==========================`)
+console.log(`                   ${input.split("\n").length} lines in input`)
+console.log(`                   Part 1: ${part1(input)}`)
+console.log(`                   Part 2: ${part2(input)}`)
